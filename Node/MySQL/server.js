3680806dd40795388c342e19b5a43b98d8b38bf4
Nicholas Mastracchio <nmastracchio@Mr-Meeseeks.local>
2015-06-26 09:10:09 -0700

    Fixed mixed spaces and tabs

--- Node/MySQL/server.js
+++ Node/MySQL/server.js
@@ -4,11 +4,11 @@ var app = express();
 
 // Create the connection to MySQL
 var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'demo_db'
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'root',
+	database: 'demo_db'
 });
 var queryString = 'SELECT * from test_table';
 
@@ -23,14 +23,14 @@ app.use(express.static('./'));
 
 // Display `index.html` when `localhost:3000` is requested
 app.get('/', function (req, res) {
-  res.sendFile('./index.html', {root: './'});
+	res.sendFile('./index.html', {root: './'});
 });
 
 // Send all records when there's a GET request to `localhost:3000/test`
 app.get('/test', function (req, res) {
 	connection.query(queryString, function(err, rows, fields) {
-	  if (err) throw err;
-	  res.send(formatData(rows));
+		if (err) throw err;
+		res.send(formatData(rows));
 	});
 });
 
@@ -40,19 +40,19 @@ var formatData = function(dataArray){
 	// Empty data store
 	var seriesData = {
 		dates: [],
-	  plot0: [],
-	  plot1: [],
-	  plot2: [],
-	  plot3: []
+		plot0: [],
+		plot1: [],
+		plot2: [],
+		plot3: []
 	};
 
-	// Push the data into the data store.
+	// Push the data into the data store. Pulls data from fields named date, plot0, plot1, plot2, plot3.
 	for(var n = 0; n < dataArray.length; n++){
 		seriesData['dates'].push((new Date(dataArray[n]['date'])).getTime());
-	  seriesData['plot0'].push(parseFloat(dataArray[n]['plot0']));
-	  seriesData['plot1'].push(parseFloat(dataArray[n]['plot1']));
-	  seriesData['plot2'].push(parseFloat(dataArray[n]['plot2']));
-	  seriesData['plot3'].push(parseFloat(dataArray[n]['plot3']));
+		seriesData['plot0'].push(parseFloat(dataArray[n]['plot0']));
+		seriesData['plot1'].push(parseFloat(dataArray[n]['plot1']));
+		seriesData['plot2'].push(parseFloat(dataArray[n]['plot2']));
+		seriesData['plot3'].push(parseFloat(dataArray[n]['plot3']));
 	}
 
 	return seriesData;
@@ -63,14 +63,14 @@ var gracefulShutdown = function(){
 	console.log("Received kill signal, shutting down gracefully.");
 	// End MySQL connection
 	connection.end(function() {
-    console.log("Disconnected from MySQL.");
-    server.close(function() {
-	    console.log("Closed out remaining connections.");
-	    process.exit()
-	  });
-  });
-  
-  // If after 10 seconds, exit anyway
+		console.log("Disconnected from MySQL.");
+		server.close(function() {
+			console.log("Closed out remaining connections.");
+			process.exit()
+		});
+	});
+	
+	// If after 10 seconds, exit anyway
 	setTimeout(function() {
 		console.error("Could not close connections in time, forcefully shutting down");
 		process.exit()
