09f0c3f69788e3d89cf2f17631455933e59d02ce
gnardecky <nardecky@zingchart.com>
2016-01-25 15:47:27 -0800

    Updating file structure, mostly getting rid of useless files

--- ASP.NET/C#/Controllers/HomeController.cs
+++ ASP.NET/C#/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace zingchartDemo.Controllers
             // Int values from DB
             int[] valuesArray = { 35, 42, 67, 89, 25, 34, 67, 85 }; 
 
-            // First way to pass data using
+            // First way to pass data
             // Serialize your data on the server side with a JSON framework for .NET
             ViewData["valuesArray"] = Newtonsoft.Json.JsonConvert.SerializeObject(valuesArray); 
                
@@ -22,7 +22,7 @@ namespace zingchartDemo.Controllers
             // Second way to pass data
             ZingChartModel myObj = new ZingChartModel(); // Create a model to hold your data
             myObj.values = valuesArray; // Assign values from DB to your model
-            return View(myObj); // Pass model into view
+            return View(myObj);         // Pass model into view
         }
     }
 }
